c515df21af3e4bc686b3a57836db8f3b5f8946ab
Shakil khatri <khatri.shakilamd@gmail.com>
2024-08-14 23:41:48 +0530

    first commit

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open("v1").then(function (cache) {
+      return cache.addAll(["./index.html", "./manifest.json", "./sw.js"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
